1367fcf462a79005936567a07aa8669016745913
Naima <nbensassi@hotmail.com>
2020-02-21 20:46:01 -0500

    add build

---
+++ client/build/precache-manifest.4d787327bc74f060d6075fa971d88afb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a25cab861b1c6bbc7d6",
+    "url": "/static/css/main.f000a1e1.chunk.css"
+  },
+  {
+    "revision": "8a25cab861b1c6bbc7d6",
+    "url": "/static/js/main.9ed4a185.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c3e2b3c6b123edaaf89",
+    "url": "/static/js/2.4ef5c4a9.chunk.js"
+  },
+  {
+    "revision": "7151b9b4810376aa6747e9e41fc8a55b",
+    "url": "/index.html"
+  }
+];
